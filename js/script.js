3014d13302d5c70767d59b5c02a1e81648a83fce
Rafal Pijet <stronglopez@wp.pl>
2019-01-25 19:22:27 +0100

    Adding example from 16.6 task

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 "use strict";
 (function () {
+    var url = "https://restcountries.eu/rest/v2/name/";
+    var coutrieslist = document.getElementById("countries");
 
+    document.getElementById("search").addEventListener("click", searchCountries);
+
+    function searchCountries() {
+        var countryName = document.getElementById("country-name").value;
+
+        if (!countryName.length) {
+            countryName = "Poland";
+        }
+        fetch(url + countryName)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(showCountriesList);
+    }
+
+    function showCountriesList(resp) {
+        coutrieslist.innerHTML = "";
+        resp.forEach(function (item) {
+            var liElement = document.createElement("li");
+            liElement.innerText = item.name + ", capital: " + item.capital;
+            coutrieslist.appendChild(liElement);
+        })
+    }
 })();
